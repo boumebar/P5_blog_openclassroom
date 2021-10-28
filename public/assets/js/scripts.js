46594a1f16e330b889c6f63eed61594285f2d39e
boumediene <boume13@hotmail.com>
2021-10-28 14:20:38 +0200

    fix error

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,9 +1,9 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
+
 (function () {
     "use strict";
   
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation');
+    var forms = document.querySelectorAll(".needs-validation");
   
     // Loop over them and prevent submission
     Array.prototype.slice.call(forms)
@@ -12,9 +12,9 @@
           if (!form.checkValidity()) {
             event.preventDefault();
             event.stopPropagation();
-          };
+          }
   
-          form.classList.add('was-validated')
+          form.classList.add("was-validated");
         }, false);
       });
   })();
